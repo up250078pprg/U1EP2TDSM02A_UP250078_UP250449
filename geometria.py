b99629716d3d4f147396edd046eea2c79772c9a6
up250078pprg <up250078@alumnos.upa.edu.mx>
2026-06-04 21:38:11 -0600

    Fase 2: Área del rectángulo

--- geometria.py
+++ geometria.py
@@ -1,5 +1,8 @@
 #Sistema de Cálculo Geométrico
 print("Módulo de Geometría Activo")
 # === ZONA DE OPERACIONES ===
-
+b=10
+h=5
+area=b*h
+print("El área del rectángulo es:", area)
 # ================================
