12f11f77c0aea1d648228494bf3be306e9537b13
up250449cgje <up250449@alumnos.upa.edu.mx>
2026-06-04 21:40:54 -0600

    Fase 3: Área del triángulo

--- geometria.py
+++ geometria.py
@@ -1,5 +1,8 @@
 #Sistema de Cálculo Geométrico
 print("Módulo de Geometría Activo")
 # === ZONA DE OPERACIONES ===
-
+b=10
+h=5
+area=(b*h)/2
+print("El área del triángulo es:", area)
 # ================================
